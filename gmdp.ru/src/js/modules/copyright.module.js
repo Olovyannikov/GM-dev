89f80b6527790f417bb5145e257bd0fa78c8092b
Ilya Olovyannikov <olovyannikov_barista@icloud.com>
2021-10-27 10:35:47 +0300

    Add footer copyright

--- gmdp.ru/src/js/modules/copyright.module.js
+++ gmdp.ru/src/js/modules/copyright.module.js
@@ -1,7 +1,9 @@
 export const copyright = () => {
     const copyrightYear = document.querySelector('#currentYear');
+    const footerCopy = document.querySelector('.footer__copy output');
 
     try {
         copyrightYear.innerHTML = `2008-${new Date().getFullYear()}`;
+        footerCopy.innerHTML = `2008-${new Date().getFullYear()}`;
     } catch (e) {}
 }
